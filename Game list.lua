35bf53cd3c9c88d00faff193e6718260fe313c11
cookieys <cookieys@hotmail.com>
2025-03-09 15:46:13 +0700

    Update Game list.lua

--- Game list.lua
+++ Game list.lua
@@ -1,1 +1,1 @@
-local a={[8540346411]="https://raw.githubusercontent.com/X7N6Y/X7N6Y/refs/heads/main/Rebirth.lua",[16732694052]="https://raw.githubusercontent.com/X7N6Y/X7N6Y/refs/heads/main/Fisch.lua"}return a
+local a={[8540346411]="https://raw.githubusercontent.com/X7N6Y/X7N6Y/refs/heads/main/Rebirth.lua",[16732694052]="https://raw.githubusercontent.com/cookieys/cookieys-hub/refs/heads/main/Fisch.lua"}return a
