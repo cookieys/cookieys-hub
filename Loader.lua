57937614b23a9b841d5f1affbf2c5b529820eac3
XryaV <cookieys@hotmail.com>
2025-03-24 22:36:37 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,17 +1,6 @@
-local gamesListURL = "https://raw.githubusercontent.com/cookieys/cookieys-hub/refs/heads/main/Game%20list.lua"
+--[[
 
-local success, Games = pcall(function()
-    return loadstring(game:HttpGet(gamesListURL))()
-end)
+discord.gg/25ms
 
-if success and Games then
-    local currentPlaceId = game.PlaceId
-    if Games[currentPlaceId] then
-        loadstring(game:HttpGet(Games[currentPlaceId]))()
-    else
-        print("Game not supported. Kicking player.")
-        game.Players.LocalPlayer:Kick("This game is not supported by the script.")
-    end
-else
-    print("Error loading games list:", Games)
-end
+--]]
+local E="https://raw.githubusercontent.com/cookieys/cookieys-hub/refs/heads/main/Game%20list.lua"local F,N=pcall(function()return(loadstring(game:HttpGet(E)))()end)if F and N then local E=game.PlaceId if N[E]then(loadstring(game:HttpGet(N[E])))()else print("Game not supported. Kicking player.")game.Players.LocalPlayer:Kick("This game is not supported by the script.")end else print("Error loading games list:",N)end
