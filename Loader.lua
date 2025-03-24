593f6f36d4d7fe92ab147f15e48726de58d39f87
XryaV <cookieys@hotmail.com>
2025-03-24 20:24:01 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/cookieys/cookieys-hub/refs/heads/main/Loader.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cookieys/cookieys-hub/refs/heads/main/Loading.lua"))()
 for pid, Execute in pairs(Games)do
 if pid==game.PlaceId then
 loadstring(game:HttpGet(Execute))()
