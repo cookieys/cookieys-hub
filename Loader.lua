aa4a3811e5526b7f324203288acbe016f7802878
XryaV <cookieys@hotmail.com>
2025-03-24 22:40:02 +0700

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,1 @@
---[[
-
-discord.gg/25ms
-
---]]
-local E="https://raw.githubusercontent.com/cookieys/cookieys-hub/refs/heads/main/Game%20list.lua"local F,N=pcall(function()return(loadstring(game:HttpGet(E)))()end)if F and N then local E=game.PlaceId if N[E]then(loadstring(game:HttpGet(N[E])))()else print("Game not supported. Kicking player.")game.Players.LocalPlayer:Kick("This game is not supported by the script.")end else print("Error loading games list:",N)end
+local a="https://raw.githubusercontent.com/cookieys/cookieys-hub/refs/heads/main/Game%20list.lua"local b,c=pcall(function()return loadstring(game:HttpGet(a))()end)if b and c then local d=game.PlaceId;if c[d]then local e,f=pcall(function()return loadstring(game:HttpGet(c[d]))()end)if not e then print("Error loading game script:",f)end end else game.Players.LocalPlayer:Kick("Error loading games list.")end
