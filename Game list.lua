ad893a458bbe30279a1721752a65925149b584ec
XryaV <cookieys@hotmail.com>
2025-04-03 10:17:43 +0700

    Update Game list.lua

--- Game list.lua
+++ Game list.lua
@@ -1,6 +1,10 @@
 local Games = {
-    [8540346411] = "https://raw.githubusercontent.com/X7N6Y/X7N6Y/refs/heads/main/Rebirth.lua",
-    [16732694052] = "https://raw.githubusercontent.com/cookieys/cookieys-hub/refs/heads/main/Fisch.lua",
+    [8540346411] = "https://raw.githubusercontent.com/X7N6Y/X7N6Y/main/Rebirth.lua",
+    [16732694052] = "https://raw.githubusercontent.com/cookieys/cookieys-hub/main/Fisch.lua",
 }
+
+if Games[game.PlaceId] then
+    loadstring(game:HttpGet(Games[game.PlaceId]))()
+end
 
 return Games
