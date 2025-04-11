8b50335d7373dd56fde644e6d2323f6b870097e4
XryaV <cookieys@hotmail.com>
2025-04-11 20:53:55 +0700

    Create Bait.lua

---
+++ Bait.lua
@@ -0,0 +1,41 @@
+return {
+    -- Common Rarity
+    ["Worm"]             = { LureSpeed = 15, Luck = 25, GenerelLuck = 0, Rarity = "Common", Resilience = 0 },
+    ["Bagel"]            = { LureSpeed = 0, Luck = 25, GenerelLuck = 0, Rarity = "Common", Resilience = 15 },
+    ["Insect"]           = { LureSpeed = 5, Luck = 35, GenerelLuck = 0, Rarity = "Common", Resilience = 0 },
+    ["Flakes"]           = { LureSpeed = 10, Luck = 55, GenerelLuck = 0, Rarity = "Common", Resilience = -3 },
+    ["Garbage"]          = { LureSpeed = -5, Luck = 0, GenerelLuck = -250, Rarity = "Common", Resilience = 50 },
+
+    -- Uncommon Rarity
+    ["Shrimp"]           = { LureSpeed = 0, Luck = 45, GenerelLuck = 25, Rarity = "Uncommon", Resilience = -5 },
+    ["Maggot"]           = { LureSpeed = -10, Luck = 0, GenerelLuck = 35, Rarity = "Uncommon", Resilience = 0 },
+
+    -- Unusual Rarity
+    ["Magnet"]           = { LureSpeed = 0, Luck = 200, GenerelLuck = 0, Rarity = "Unusual", Resilience = 0 },
+    ["Peppermint Worm"]  = { LureSpeed = -5, Luck = 50, GenerelLuck = 30, Rarity = "Unusual", Resilience = 20 },
+    ["Squid"]            = { LureSpeed = -25, Luck = 55, GenerelLuck = 45, Rarity = "Unusual", Resilience = 0 },
+    ["Seaweed"]          = { LureSpeed = 20, Luck = 35, GenerelLuck = 0, Rarity = "Unusual", Resilience = 10 },
+    ["Coral"]            = { LureSpeed = 20, Luck = 0, GenerelLuck = 0, Rarity = "Unusual", Resilience = 20 },
+    ["Minnow"]           = { LureSpeed = 0, Luck = 65, GenerelLuck = 0, Rarity = "Unusual", Resilience = -10 },
+
+    -- Rare Rarity
+    ["Coal"]             = { LureSpeed = 0, Luck = 45, GenerelLuck = 0, Rarity = "Rare", Resilience = -10 },
+    ["Super Flakes"]     = { LureSpeed = 0, Luck = 0, GenerelLuck = 70, Rarity = "Rare", Resilience = -15 },
+    ["Rapid Catcher"]    = { LureSpeed = 35, Luck = 0, GenerelLuck = 0, Rarity = "Rare", Resilience = -15 },
+
+    -- Legendary Rarity
+    ["Fish Head"]        = { LureSpeed = 10, Luck = 150, GenerelLuck = 0, Rarity = "Legendary", Resilience = -10 },
+    ["Night Shrimp"]     = { LureSpeed = 15, Luck = 0, GenerelLuck = 90, Rarity = "Legendary", Resilience = 0 },
+    ["Instant Catcher"]  = { LureSpeed = 65, Luck = 0, GenerelLuck = -20, Rarity = "Legendary", Resilience = -15 },
+    ["Kraken Tentacle"]  = { LureSpeed = 35, Luck = 100, GenerelLuck = 35, Rarity = "Legendary", Resilience = 15 },
+    ["Holly Berry"]      = { LureSpeed = -5, Luck = 80, GenerelLuck = 30, Rarity = "Legendary", Resilience = 10 },
+    ["Deep Coral"]       = { LureSpeed = 0, Luck = -10, GenerelLuck = 0, Rarity = "Legendary", Resilience = 50 },
+    ["Weird Algae"]      = { LureSpeed = -35, Luck = 0, GenerelLuck = 200, Rarity = "Legendary", Resilience = 0 },
+    ["Truffle Worm"]     = { LureSpeed = -10, Luck = 300, GenerelLuck = 0, Rarity = "Legendary", Resilience = 0 },
+
+    -- Mythical Rarity
+    ["Chocolate Fish"]   = { LureSpeed = 20, Luck = 200, GenerelLuck = 150, Rarity = "Mythical", Resilience = 15, Mutation = "Chocolate" },
+    ["Hangman's Hook"]   = { ProgressSpeed = 45, LureSpeed = -5, Luck = 150, GenerelLuck = 35, Rarity = "Mythical", Resilience = 20 },
+    ["Shark Head"]       = { LureSpeed = -5, Luck = 225, GenerelLuck = 30, Rarity = "Mythical", Resilience = 10 },
+    ["Aurora Bait"]      = { LureSpeed = -5, Luck = 100, GenerelLuck = 30, Rarity = "Mythical", Resilience = 10 },
+}
